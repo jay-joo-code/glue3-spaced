b17e5901c780cd14229a0c9b8a8ed3d86c2e4537
Jay Joo <jj534@cornell.edu>
2023-04-03 16:58:00 -0400

    add feature list section, minor layout improvements

--- src/lib/components/glue/landing/SectionBoldStatement.svelte
+++ src/lib/components/glue/landing/SectionBoldStatement.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import IconRightArrowLong from '$lib/icons/glue/IconRightArrowLong.svelte';
 	import RadialGradient from './RadialGradient.svelte';
 
 	export let overline: string = 'Our mission';
 	export let heading: string = 'Give your schedulers a calendar not a list';
 	export let desc: string =
 		'I’ve written on why traditional “semantic class names” are the reason CSS is hard to maintain, but the truth is you’re never going to believe me until you actually try it. If you can suppress the urge to retch long enough to give it a chance, I really think you’ll wonder how you ever worked with CSS any other way.';
+	export let linkLabel: string = '';
+	export let linkHref: string = '';
 </script>
 
 <section class="relative overflow-hidden px-8 py-16 text-center md:pt-48">
@@ -13,7 +16,14 @@
 	<h2 class="mt-4 text-4xl font-extrabold tracking-tight sm:text-5xl">
 		{heading}
 	</h2>
-	<p class="mx-auto mt-10 max-w-3xl text-lg text-base-content/70">
-		{desc}
-	</p>
+	{#if desc}
+		<p class="mx-auto mt-10 max-w-3xl text-lg text-base-content/70">
+			{desc}
+		</p>
+	{/if}
+	{#if linkLabel}
+		<a class="btn-primary btn mt-10 gap-1" href={linkHref} target="_blank" rel="noreferrer"
+			>{linkLabel} <span class="text-xl"><IconRightArrowLong /></span></a
+		>
+	{/if}
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import SectionBoldStatement from '$lib/components/glue/landing/SectionBoldStatement.svelte';
+	import SectionFeatureList from '$lib/components/glue/landing/SectionFeatureList.svelte';
 	import SectionHero from '$lib/components/glue/landing/SectionHero.svelte';
 	import SectionInfo from '$lib/components/glue/landing/SectionInfo.svelte';
 	import PageContainer from '$lib/components/glue/PageContainer.svelte';
+	import IconRightArrowLong from '$lib/icons/glue/IconRightArrowLong.svelte';
 </script>
 
 <PageContainer title="Home">
 	<SectionHero />
 	<SectionBoldStatement />
+	<SectionFeatureList />
 	<SectionInfo />
 	<SectionInfo isReversed={true} />
+	<SectionBoldStatement
+		overline="Get started"
+		heading="Ready to get started with our calendars?"
+		desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nisi debitis in autem iure distinctio omnis sapiente laboriosam id ipsa!"
+		linkLabel="Get started"
+		linkHref="/"
+	/>
 </PageContainer>
