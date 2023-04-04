4027d407a7ad188ab15f578c1a981bc055b80979
Jay Joo <jj534@cornell.edu>
2023-04-04 14:49:47 -0400

    update default auth config

--- src/lib/glue/config.ts
+++ src/lib/glue/config.ts
@@ -2,7 +2,6 @@ import type { IPublicNav } from '$lib/types/glue/nav.type';
 
 export const APP_NAME = 'AppName';
 export const PUBLIC_NAVS: IPublicNav[] = [];
-export const IS_GOOGLE_AUTH_ONLY = true;
-export const IS_ENFORCE_CORNELL_EMAIL = true;
+export const IS_ENFORCE_CORNELL_EMAIL = false;
 export const IS_BETA = true;
 export const SENTRY_DSN_PUBLIC = '';
