fabdb71eeee17f7553ec97473f07e759e2369903
Jay Joo <jj534@cornell.edu>
2023-04-05 15:21:31 -0400

    add toast infrastructure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { supabase } from '$lib/glue/supabaseClient';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 
 	onMount(() => {
 		const {
@@ -15,8 +16,31 @@
 			subscription.unsubscribe();
 		};
 	});
+
+	const toastOptions = {
+		dismissable: false,
+		theme: {
+			'--toastContainerRight': '1rem',
+			'--toastBarHeight': '0',
+			'--toastPadding': '0.2rem 0.6rem',
+			'--toastMsgPadding': '0.2rem 0.2rem 0.2rem 0.4rem',
+			'--toastBorderRadius': '0.5rem',
+			'--toastMinHeight': '0',
+			'--toastWidth': 'auto'
+		}
+	};
 </script>
+
+<div class="toast-styles">
+	<SvelteToast options={toastOptions} />
+</div>
 
 <AppLayout>
 	<slot />
 </AppLayout>
+
+<style>
+	.toast-styles {
+		font-size: 0.9rem;
+	}
+</style>
