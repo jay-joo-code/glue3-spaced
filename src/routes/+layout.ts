34505985e61dae344d35a48ec4ec74c07ca8c96c
Jay Joo <jj534@cornell.edu>
2023-04-20 11:23:31 -0400

    add profile to layout page data

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,15 @@
+import { supabase } from '$lib/glue/supabaseClient';
 import type { LayoutLoad } from './$types';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
-	return { session };
+
+	const { data: profile } = await supabase
+		.from('profiles')
+		.select('*')
+		.eq('id', session?.user?.id)
+		.single();
+
+	return { session, profile };
 };
