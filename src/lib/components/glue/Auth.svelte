2f030ec7b5b8a95207e4d6e35cc802ec44bb2959
Jay Joo <jj534@cornell.edu>
2023-05-04 00:57:10 -0400

    remove console log

--- src/lib/components/glue/Auth.svelte
+++ src/lib/components/glue/Auth.svelte
@@ -76,8 +76,6 @@
 		magicLinkState = 'sent';
 		isAuthLoading = false;
 	}
-
-	console.log('$page?.data?.session', $page?.data?.session);
 </script>
 
 {#if $page?.data?.session}
@@ -86,13 +84,13 @@
 		<label tabindex="0" class="btn-ghost btn-square btn">
 			<div class="placeholder avatar">
 				<div
-					class="w-8 rounded-full bg-neutral-focus text-neutral-content ring-2 ring ring-primary ring-offset-2 ring-offset-base-100"
-				>
+					class="w-8 rounded-full bg-neutral-focus text-neutral-content ring-2 ring ring-primary ring-offset-2 ring-offset-base-100">
 					{#if $page?.data?.session?.user?.user_metadata?.avatar_url}
 						<img src={$page?.data?.session?.user?.user_metadata?.avatar_url} />
 					{:else if $page?.data?.session?.user?.user_metadata?.name}
-						<span class="text-sm">{$page?.data?.session?.user?.user_metadata?.name?.charAt(0)}</span
-						>
+						<span class="text-sm">
+							{$page?.data?.session?.user?.user_metadata?.name?.charAt(0)}
+						</span>
 					{:else}
 						<div class="text-xl">
 							<IconPerson />
@@ -103,8 +101,7 @@
 		</label>
 		<ul
 			tabindex="0"
-			class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-200 p-2 shadow"
-		>
+			class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-200 p-2 shadow">
 			{#each PRIVATE_NAVS as nav}
 				<li>
 					<a href={nav.path}>
@@ -130,11 +127,11 @@
 					<!-- force Cornell auth -->
 					<h3 class="p-0 text-xl font-medium">
 						Sign in
-						<span
-							>with your <span class="underline decoration-primary underline-offset-2"
-								>Cornell&nbsp;email</span
-							></span
-						>
+						<span>
+							with your <span class="underline decoration-primary underline-offset-2">
+								Cornell&nbsp;email
+							</span>
+						</span>
 					</h3>
 					<p class="mb-2 text-sm text-base-content/80">
 						Access all of the features by signing in and getting started with {APP_NAME}.
@@ -142,11 +139,9 @@
 					<p class="mb-2 text-sm text-base-content/80">
 						You must sign in with your Cornell email to sign into {APP_NAME}!
 					</p>
-					<button type="button" class="btn-primary btn mt-2 gap-2" on:click={signInWithGoogle}
-						><IconGoogle /> Sign in with {IS_ENFORCE_CORNELL_EMAIL
-							? 'Cornell email'
-							: 'Google'}</button
-					>
+					<button type="button" class="btn-primary btn mt-2 gap-2" on:click={signInWithGoogle}>
+						<IconGoogle /> Sign in with {IS_ENFORCE_CORNELL_EMAIL ? 'Cornell email' : 'Google'}
+					</button>
 				{:else if authMethod === 'magic-link'}
 					{#if magicLinkState === 'not-sent'}
 						<form class="pb-4" on:submit={signInEmailMagicLink}>
@@ -159,13 +154,12 @@
 									type="email"
 									name="email"
 									required
-									bind:value={email}
-								/>
+									bind:value={email} />
 							</div>
 							<button
-								class="btn-primary btn-block btn mt-6 rounded-full {isAuthLoading && 'loading'}"
-								>Sign in with magic link</button
-							>
+								class="btn-primary btn-block btn mt-6 rounded-full {isAuthLoading && 'loading'}">
+								Sign in with magic link
+							</button>
 						</form>
 					{:else if magicLinkState === 'sent'}
 						<!-- magic link sent -->
@@ -175,8 +169,9 @@
 						</p>
 						<button
 							class="btn-block btn mt-4 rounded-full {isAuthLoading && 'loading'}"
-							on:click={signInEmailMagicLink}>Resend magic link</button
-						>
+							on:click={signInEmailMagicLink}>
+							Resend magic link
+						</button>
 					{/if}
 				{:else if authMethod === 'email'}
 					<!-- email pwd auth -->
@@ -193,8 +188,7 @@
 									name="email"
 									placeholder="name@company.com"
 									required
-									bind:value={email}
-								/>
+									bind:value={email} />
 							</div>
 							<div class="form-control">
 								<label class="label" for="password">Password</label>
@@ -204,32 +198,31 @@
 									name="password"
 									placeholder=""
 									required
-									bind:value={password}
-								/>
+									bind:value={password} />
 							</div>
 							<div class="flex items-center justify-between">
 								{#if state === 'signin'}
 									<div>
 										<a
 											href="/"
-											class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-											>Lost password?</a
-										>
+											class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">
+											Lost password?
+										</a>
 									</div>
 								{/if}
 							</div>
 							{#if authError}
 								<p class="text-sm text-error">{authError}</p>
 							{/if}
-							<button type="submit" class="btn-primary btn-block btn"
-								>{state === 'signin' ? 'Sign in' : 'Create account'}</button
-							>
+							<button type="submit" class="btn-primary btn-block btn">
+								{state === 'signin' ? 'Sign in' : 'Create account'}
+							</button>
 							<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
 								{state === 'signin' ? 'Not registered?' : 'Already have an account?'}
 								<button
 									class="text-blue-700 hover:underline dark:text-blue-500"
-									on:click={toggleState}
-									>{state === 'signin' ? 'Create account' : 'Sign in'}
+									on:click={toggleState}>
+									{state === 'signin' ? 'Create account' : 'Sign in'}
 								</button>
 							</div>
 						</div>
