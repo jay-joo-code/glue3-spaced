b88c2fd407349091e18c8cd1e2977ef923768ce0
Jay Joo <jj534@cornell.edu>
2023-05-04 13:50:55 -0400

    add option to send email by user id

--- src/routes/api/email/+server.ts
+++ src/routes/api/email/+server.ts
@@ -1,19 +1,40 @@
-import { SENDGRID_API_KEY, SENDGRID_VERIFIED_SENDER } from '$env/static/private';
+import {
+	SENDGRID_API_KEY,
+	SENDGRID_VERIFIED_SENDER,
+	SUPABSE_SERVICE_ROLE_KEY
+} from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import sgMail from '@sendgrid/mail';
 import { json } from '@sveltejs/kit';
+import { createClient } from '@supabase/supabase-js';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-	const { to, subject, html } = await request.json();
+	const { recipientUserId, ...emailConfig } = await request.json();
 
 	sgMail.setApiKey(SENDGRID_API_KEY);
 
-	await sgMail.send({
-		to,
-		from: SENDGRID_VERIFIED_SENDER,
-		subject,
-		html
-	});
+	if (recipientUserId) {
+		const supabase = createClient(PUBLIC_SUPABASE_URL, SUPABSE_SERVICE_ROLE_KEY, {
+			auth: {
+				autoRefreshToken: false,
+				persistSession: false
+			}
+		});
+
+		const { data } = await supabase.auth.admin.getUserById(recipientUserId);
+
+		await sgMail.send({
+			...emailConfig,
+			from: SENDGRID_VERIFIED_SENDER,
+			to: data?.user?.email
+		});
+	} else {
+		await sgMail.send({
+			...emailConfig,
+			from: SENDGRID_VERIFIED_SENDER
+		});
+	}
 
 	return json({
 		isSent: true
