4f695c00134778383175b8255e20f2b821848dc8
Jay Joo <jj534@cornell.edu>
2023-05-21 13:03:54 -0400

    remove unused import

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import SectionBoldStatement from '$lib/components/glue/landing/SectionBoldStatement.svelte';
 	import SectionFeatureList from '$lib/components/glue/landing/SectionFeatureList.svelte';
 	import SectionHero from '$lib/components/glue/landing/SectionHero.svelte';
 	import SectionInfo from '$lib/components/glue/landing/SectionInfo.svelte';
 	import PageContainer from '$lib/components/glue/PageContainer.svelte';
-	import IconRightArrowLong from '$lib/icons/glue/IconRightArrowLong.svelte';
 </script>
 
 <PageContainer title="Home">
@@ -18,6 +18,5 @@
 		heading="Ready to get started with our calendars?"
 		desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nisi debitis in autem iure distinctio omnis sapiente laboriosam id ipsa!"
 		linkLabel="Get started"
-		linkHref="/"
-	/>
+		linkHref="/" />
 </PageContainer>
