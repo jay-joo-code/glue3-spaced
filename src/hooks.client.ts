c5f789dc1ecc666d0c07c731a1cf66b35bac42ac
Jay Joo <jj534@cornell.edu>
2023-07-04 20:15:35 -0500

    default sentry to capturing detailed network data

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -16,7 +16,11 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0,
 
 	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [new Replay()]
+	integrations: [
+		new Replay({
+			networkDetailAllowUrls: [window.location.origin]
+		})
+	]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
