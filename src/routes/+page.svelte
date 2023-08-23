c78117942e9a9425ef473100fbd5c12048038628
Jay Joo <jj534@cornell.edu>
2023-08-23 17:38:15 -0400

    add search cards feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
 	import { toast } from '@zerodevx/svelte-toast';
 	import { invalidateAll } from '$app/navigation';
 
+	let searchQuery = '';
+	let searchResultCards = [];
+
 	$: ({ todayFlashcards, upcomingFlashcards, supabase } = $page.data);
 
 	const addCard = async () => {
@@ -22,29 +25,68 @@
 		if (error) toast.push('There was an error with creating a flashcard');
 		else invalidateAll();
 	};
+
+	const fetchSearchResults = async () => {
+		const { data, error } = await supabase
+			.from('flashcard')
+			.select()
+			.textSearch('body', searchQuery, {
+				config: 'english'
+			});
+		if (error) toast.push('There was an error with search for flashcards');
+		else {
+			searchResultCards = data;
+		}
+	};
+
+	const resetSearchResults = () => {
+		if (searchResultCards?.length > 0) {
+			searchResultCards = [];
+		}
+	};
 </script>
 
 <PageContainer title="Home" layout="mobile-only">
 	<div class="flex w-full justify-center">
 		<div class="w-full max-w-3xl">
-			<div class="flex items-center justify-between">
-				<h2 class="text-3xl font-extrabold">Today</h2>
-				<button class="btn-secondary btn-sm btn" on:click={addCard}><IconAdd /> Add card</button>
-			</div>
+			<form
+				class="flex items-center rounded-full border border-base-content/20 p-2"
+				on:submit={fetchSearchResults}>
+				<input
+					type="text"
+					placeholder="Search for a card"
+					class="input input-sm w-full"
+					bind:value={searchQuery}
+					on:input={resetSearchResults} />
+				<button class="btn-secondary btn-sm btn rounded-full">Search</button>
+			</form>
 
-			<div class="mt-6 space-y-4">
-				{#each todayFlashcards as flashcard (flashcard?.id)}
-					<Flashcard {flashcard} />
-				{/each}
-			</div>
+			{#if searchQuery?.length > 0}
+				<div class="mt-6 space-y-4">
+					{#each searchResultCards as flashcard (flashcard?.id)}
+						<Flashcard {flashcard} />
+					{/each}
+				</div>
+			{:else}
+				<div class="mt-8 flex items-center justify-between">
+					<h2 class="text-3xl font-extrabold">Today</h2>
+					<button class="btn-secondary btn-sm btn" on:click={addCard}><IconAdd /> Add card</button>
+				</div>
 
-			<h2 class="mt-10 text-3xl font-extrabold">Upcoming</h2>
-			<div class="relative mt-6 space-y-4">
-				{#each upcomingFlashcards as flashcard (flashcard?.id)}
-					<Flashcard {flashcard} />
-				{/each}
-				<div class="absolute bottom-0 w-full bg-gradient-to-t from-base-100 pt-64" />
-			</div>
+				<div class="mt-6 space-y-4">
+					{#each todayFlashcards as flashcard (flashcard?.id)}
+						<Flashcard {flashcard} />
+					{/each}
+				</div>
+
+				<h2 class="mt-10 text-3xl font-extrabold">Upcoming</h2>
+				<div class="relative mt-6 space-y-4">
+					{#each upcomingFlashcards as flashcard (flashcard?.id)}
+						<Flashcard {flashcard} />
+					{/each}
+					<div class="absolute bottom-0 w-full bg-gradient-to-t from-base-100 pt-64" />
+				</div>
+			{/if}
 		</div>
 	</div>
 </PageContainer>
